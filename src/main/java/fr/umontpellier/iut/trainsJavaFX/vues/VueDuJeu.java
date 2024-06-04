4996d0ba4fa14b59c8319c8bc27d7449eb31b02d
Goharik <goharik.gyurjyan@etu.umontpellier.fr>
2024-06-04 14:26:12 +0200

    debut fxml

--- src/main/java/fr/umontpellier/iut/trainsJavaFX/vues/VueDuJeu.java
+++ src/main/java/fr/umontpellier/iut/trainsJavaFX/vues/VueDuJeu.java
@@ -13,6 +13,7 @@ import javafx.collections.ListChangeListener;
 import javafx.collections.ObservableList;
 import javafx.event.Event;
 import javafx.event.EventHandler;
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
@@ -37,11 +38,18 @@ import java.io.IOException;
 public class VueDuJeu extends BorderPane {
 
     private final IJeu jeu;
+    @FXML
     private VuePlateau plateau;
+    @FXML
     private Label instruction;
+    @FXML
     private Label nomJoueur;
+    @FXML
     private Button passer;
-    private ObjectProperty<IJoueur> joueurCourantProperty;
+
+    private ObjectProperty<IJoueur> joueurCourantProperty = new SimpleObjectProperty<>();;
+
+    @FXML
     private VueJoueurCourant joueurCourant;
 
     public VueDuJeu(IJeu jeu) {
@@ -54,18 +62,6 @@ public class VueDuJeu extends BorderPane {
             e.printStackTrace();
         }
         this.jeu = jeu;
-        plateau = new VuePlateau();
-        instruction = new Label();
-        nomJoueur = new Label();
-        passer = new Button("passer");
-        joueurCourantProperty = new SimpleObjectProperty<>();
-        joueurCourant = new VueJoueurCourant();
-
-        setTop(instruction);
-        setCenter(plateau);
-        setRight(joueurCourant);
-        setLeft(passer);
-        setBottom(joueurCourant.getCartesEnMain());
     }
     public void creerBindings() {
         joueurCourantProperty.bind(jeu.joueurCourantProperty());
