e469fcfee02d2b9b4407bc2a51744e4ac82b353f
Cano Corentin <corentin.cano@etu.umontpellier.fr>
2024-06-16 21:37:23 +0200

    Implementation Resultats de fin

--- src/main/java/fr/umontpellier/iut/trainsJavaFX/TrainsIHM.java
+++ src/main/java/fr/umontpellier/iut/trainsJavaFX/TrainsIHM.java
@@ -12,12 +12,15 @@ import javafx.application.Platform;
 import javafx.collections.ListChangeListener;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.control.Alert;
+import javafx.scene.control.ButtonType;
 import javafx.stage.Screen;
 import javafx.stage.Stage;
 
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
+import java.util.Optional;
 
 public class TrainsIHM extends Application {
     private VueChoixJoueurs vueChoixJoueurs;
@@ -61,6 +64,7 @@ public class TrainsIHM extends Application {
 
         Scene scene = new Scene(vueDuJeu, Screen.getPrimary().getBounds().getWidth() * DonneesGraphiques.pourcentageEcran, Screen.getPrimary().getBounds().getHeight() * DonneesGraphiques.pourcentageEcran); // la scene doit être créée avant de mettre en place les bindings
         vueDuJeu.creerBindings();
+
         jeu.finDePartieProperty().addListener((source, ancien, nouveau) -> {
             if (nouveau) {
                 arreterJeu();
@@ -69,8 +73,6 @@ public class TrainsIHM extends Application {
             }
         });
         jeu.run(); // le jeu doit être démarré après que les bindings ont été mis en place
-
-
 
         primaryStage.setMinWidth(Screen.getPrimary().getBounds().getWidth() / 2.5);
         primaryStage.setMinHeight(Screen.getPrimary().getBounds().getHeight() / 2.5);
@@ -93,13 +95,13 @@ public class TrainsIHM extends Application {
     };
 
     public void arreterJeu() {
-/*        Alert alert = new Alert(Alert.AlertType.CONFIRMATION);
+        Alert alert = new Alert(Alert.AlertType.CONFIRMATION);
         alert.setTitle("Confirmation");
         alert.setContentText("On arrête de jouer ?");
         Optional<ButtonType> result = alert.showAndWait();
-        if (result.isPresent() && result.get() == ButtonType.OK) {*/
+        if (result.isPresent() && result.get() == ButtonType.OK) {
             Platform.exit();
-//        }
+        }
     }
 
     public Jeu getJeu() {
--- src/main/java/fr/umontpellier/iut/trainsJavaFX/vues/VueResultats.java
+++ src/main/java/fr/umontpellier/iut/trainsJavaFX/vues/VueResultats.java
@@ -1,14 +1,56 @@
 package fr.umontpellier.iut.trainsJavaFX.vues;
 
+import fr.umontpellier.iut.trainsJavaFX.IJoueur;
 import fr.umontpellier.iut.trainsJavaFX.TrainsIHM;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.geometry.Pos;
+import javafx.scene.control.Label;
+import javafx.scene.layout.HBox;
 import javafx.scene.layout.Pane;
+import javafx.scene.layout.VBox;
+import javafx.scene.paint.Paint;
 
-public class VueResultats extends Pane {
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+import java.util.TreeSet;
+
+public class VueResultats extends VBox {
 
     private TrainsIHM ihm;
 
+    @FXML
+    VBox boiteClassement;
+
     public VueResultats(TrainsIHM ihm) {
         this.ihm = ihm;
+        try {
+            FXMLLoader loader = new FXMLLoader(getClass().getClassLoader().getResource("fxml/resultats.fxml"));
+            loader.setRoot(this);
+            loader.setController(this);
+            loader.load();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        for (IJoueur joueur : getClassement()){
+            HBox boiteJoueur = new HBox();
+            Label labelJoueur = new Label(joueur.getNom());
+            labelJoueur.setTextFill(Paint.valueOf(CouleursJoueurs.couleursBackgroundJoueur.get(joueur.getCouleur())));
+            Label labelScore = new Label(" : " + joueur.getScoreTotal());
+            boiteJoueur.getChildren().addAll(labelJoueur,labelScore);
+            boiteJoueur.setAlignment(Pos.CENTER);
+            boiteClassement.getChildren().add(boiteJoueur);
+        }
     }
+
+    public List<IJoueur> getClassement() {
+        List<IJoueur> classement = new ArrayList<>(ihm.getJeu().getJoueurs());
+        classement.sort((j1, j2) -> j2.getScoreTotal()-j1.getScoreTotal());
+        return classement;
+    }
+
+
 
 }
